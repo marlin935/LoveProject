9c597315f4073d3fa4c78b0fe2b32b2ad51da946
marlin935 <164975422+marlin935@users.noreply.github.com>
2026-07-12 22:25:09 +0530

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -9,8 +9,7 @@ export default async function handler(req, res) {
   const token = process.env.TELEGRAM_BOT_TOKEN;
   const chatId = process.env.TELEGRAM_CHAT_ID;
 
-  const url =
-  https://api.telegram.org/bot${token}/sendMessage;
+  const url = 'https://api.telegram.org/bot${token}/sendMessage;
 
   await fetch(url,{
     method:"POST",
